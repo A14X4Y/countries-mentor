eebd8c2b19ae56bde59197baa8b44b4844502f4f
Alexey Belousov <leshabelousov2001@gmail.com>
2022-12-18 15:44:01 +0500

    полностью сделанная страница Details

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,20 +1,41 @@
 import { useNavigate, useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
 import { IoArrowBack } from "react-icons/io5";
 
 import { Button } from "../components/Button";
 import { Info } from "../components/Info";
+import {
+  selectCurrentCountry,
+  selectDetails,
+} from "../store/details/details-selector";
+import {
+  clearDetails,
+  loadCountryByName,
+} from "../store/details/details-action";
+import Preloader from "../components/preloader/Preloader";
 
 export const Details = () => {
   const { name } = useParams();
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-  const currentCountry = null;
+  const { currentCountry, error, status } = useSelector(selectDetails);
+
+  useEffect(() => {
+    dispatch(loadCountryByName(name));
+    return () => {
+      dispatch(clearDetails());
+    };
+  }, [name, dispatch]);
 
   return (
     <div>
       <Button onClick={() => navigate(-1)}>
         <IoArrowBack /> Back
       </Button>
+      {status === "loading" && <Preloader />}
+      {error === "error" && <h2>{error}</h2>}
       {currentCountry && <Info push={navigate} {...currentCountry} />}
     </div>
   );
---
+++ src/store/details/details-reducer.js
@@ -0,0 +1,57 @@
+import {
+  SET_COUNTRY,
+  SET_ERROR,
+  SET_LOADING,
+  CLEAR_DETAILS,
+  SET_NEIGHBORS,
+} from "./details-action";
+
+const initialState = {
+  currentCountry: null,
+  status: "idle", // loading | received | rejected
+  error: null,
+  neighbors: [],
+};
+
+export const detailsReducer = (state = initialState, { type, payload }) => {
+  switch (type) {
+    case SET_COUNTRY: {
+      return {
+        ...state,
+        status: "received",
+        currentCountry: payload,
+      };
+    }
+
+    case SET_ERROR: {
+      return {
+        ...state,
+        status: "rejected",
+        error: payload,
+      };
+    }
+
+    case SET_LOADING: {
+      return {
+        ...state,
+        status: "loading",
+        error: null,
+      };
+    }
+
+    case CLEAR_DETAILS: {
+      return initialState;
+    }
+
+    case SET_NEIGHBORS: {
+      return {
+        ...state,
+        neighbors: payload,
+      };
+    }
+
+    default: {
+      return initialState;
+    }
+  }
+};
